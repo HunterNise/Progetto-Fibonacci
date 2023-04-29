6a926f71c975420186b8f38d60156ee2c88e300e
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-04-29 23:11:06 +0200

    Add files via upload

---
+++ capitolo 2/prova del 9/prova_del_9.c
@@ -0,0 +1,60 @@
+#include "../../mylib.h"
+
+
+int rem (struct NUMBER num) {
+	int l = num.length;
+	int s = 0;
+
+	for (int k = 0; k < l; k++) {
+		s += num.digits[k];
+	}
+	while (s > 9) {
+		s -= 9;
+	}
+	return s;
+}
+
+int cast_out_9 (struct NUMBER num1, struct NUMBER num2, struct NUMBER result, int sel) {
+	int r1 = rem (num1);
+	int r2 = rem (num2);
+	int rr = rem (result);
+
+	switch (sel) {
+		case 0:
+			return (r1 + r2) == rr;
+		case 1:
+			return (r1 * r2) == rr;
+	}
+}
+
+
+int main (void) {
+	setbuf (stdout, NULL);
+
+
+	int n1, n2, r, sel;
+	struct NUMBER num1, num2, result;
+
+	printf ("somma (0) o prodotto (1): ");
+	scanf ("%d", &sel);
+
+	printf("primo numero: ");
+	scanf ("%d", &n1);
+	printf("secondo numero: ");
+	scanf ("%d", &n2);
+	printf ("risultato: ");
+	scanf ("%d", &r);
+
+	num1 = init (n1);
+	num2 = init (n2);
+	result = init (r);
+
+	int a = cast_out_9 (num1, num2, result, sel);
+	printf ("prova del 9: %d", a);
+
+
+	free (num1.digits);
+	free (num2.digits);
+	free (result.digits);
+	return 0;
+}
---
+++ capitolo 3/somma/somma.c
@@ -0,0 +1,47 @@
+#include "../../mylib.h"
+
+
+struct NUMBER sum (struct NUMBER num1, struct NUMBER num2) {
+	struct NUMBER result;
+	int l1 = num1.length, l2 = num2.length;
+	int lmax = max (num1.length, num2.length) + 1;
+
+	int* d = calloc (lmax, sizeof(int));
+	int carry = 0, c1 = 0, c2 = 0;
+	for (int k = 0; k < lmax; k++) {
+		c1 = (k < l1) ? num1.digits[k] : 0 ; // check if it's within the boundary of the array
+		c2 = (k < l2) ? num2.digits[k] : 0 ;
+
+		int s = c1 + c2 + carry;
+		d[k] = get_last_digit (s);
+		carry = (int) (s / 10);
+	}
+	result.digits = d;
+
+	int k = lmax-1;
+	while (d[k] == 0 && k >= 0) { // ignore trailing 0s (0s at the beginning of the number)
+		k--;
+	}
+	result.length = (k >= 0) ? k+1 : 1 ; // exception: if result = 0 then k = -1, but length should be 1
+
+	return result;
+}
+
+
+int main (void) {
+	int n1, n2;
+	struct NUMBER num1, num2;
+
+	scanf ("%d%d", &n1, &n2);
+	num1 = init (n1);
+	num2 = init (n2);
+
+	struct NUMBER result = sum (num1, num2);
+	print (result);
+
+
+	free (num1.digits);
+	free (num2.digits);
+	free (result.digits);
+	return 0;
+}
