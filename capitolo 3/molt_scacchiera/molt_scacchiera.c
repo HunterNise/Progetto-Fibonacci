826aeb9b1dc421d347d7733c027befd932afc26e
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-05-02 23:16:11 +0200

    Add files via upload

---
+++ capitolo 3/molt_scacchiera/molt_scacchiera.c
@@ -0,0 +1,98 @@
+#include "../../mylib.h"
+
+
+struct NUMBER matrix_mult (struct NUMBER num1, struct NUMBER num2) {
+	// initialize variables
+	struct NUMBER result;
+	int l1 = num1.length, l2 = num2.length;
+	int lmax = num1.length + num2.length; // maximum length of the result
+	int lrow = l1 + 1; // length of the matrix's rows
+
+	// allocate memory
+	int* d = calloc (lmax, sizeof(int)); // result digits
+	int** M = (int**) calloc (l2, sizeof(int*)); // matrix of intermediate products
+	for (int k = 0; k < l2; k++) {
+		M[k] = (int*) calloc (lrow, sizeof(int));
+	}
+
+	// intermediate products
+	int carry = 0, c1 = 0, c2 = 0, s = 0;
+	for (int j = 0; j < l2; j++) {
+
+		carry = 0;
+		for (int i = 0; i < lrow; i++) {
+			c1 = (i < l1) ? num1.digits[i] : 0 ; // check if it's within the boundary of the array
+			c2 = (j < l2) ? num2.digits[j] : 0 ;
+
+			s = c1 * c2 + carry;
+
+			M[j][i] = get_last_digit (s);
+			carry = (int) (s / 10);
+		}
+	}
+
+	printf ("\n");
+	for (int j = 0; j < l2; j++) {
+		for (int i = lrow-1; i >= 0; i--) {
+			printf ("%d", M[j][i]);
+		}
+		printf ("\n");
+	}
+	printf ("\n");
+
+
+	// diagonal sum
+	int c_j = 0;
+	carry = 0;
+	for (int k = 0; k < lmax; k++) {
+
+		s = 0;
+		printf ("%d  ", carry);
+		for (int j = 0; j < l2; j++) {
+			c_j = ((k-j >= 0) && (k-j < l1+1)) ? M[j][k-j] : 0 ; // check if it's within the boundary of the array
+			printf ("%d ", c_j);
+			s += c_j;
+		}
+		s += carry;
+		printf (" = %d ", s);
+
+		d[k] = get_last_digit (s);
+		carry = (int) (s / 10);
+		printf ("\n");
+	}
+	printf ("\n");
+	result.digits = d;
+
+	// calculate result length
+	int k = lmax-1;
+	while (d[k] == 0 && k >= 0) { // ignore trailing 0s (0s at the beginning of the number)
+		k--;
+	}
+	result.length = (k >= 0) ? k+1 : 1 ; // exception: if result = 0 then k = -1, but length should be 1
+
+	// free allocated memory
+	for (int k = 0; k < l2; k++) {
+		free (M[k]);
+	}
+	free (M);
+	return result;
+}
+
+
+int main (void) {
+	int n1, n2;
+	struct NUMBER num1, num2;
+
+	scanf ("%d%d", &n1, &n2);
+	num1 = init (n1);
+	num2 = init (n2);
+
+	struct NUMBER result = matrix_mult (num1, num2);
+	print (result);
+
+
+	free (num1.digits);
+	free (num2.digits);
+	free (result.digits);
+	return 0;
+}
