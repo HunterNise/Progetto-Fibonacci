820b657422f6604fd2f459b716fe3279806da2ed
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-07-19 14:04:49 +0200

    Add files via upload

--- capitolo 5/infilzare/infilzare.c
+++ capitolo 5/infilzare/infilzare.c
@@ -1,65 +1,10 @@
 #include "../../mylib.h"
 
 
-struct FRACTION mult_to_simple (struct multFRACTION frac) {
-	// initialize variables
-	struct FRACTION result;
-	int l = frac.length;
-
-	int a, b;
-	if (frac.circle < 0) { // no circle
-		b = 1;
-		for (int k = 0; k < l; k++) {
-			b *= frac.den[k];
-		}
-
-		a = 0;
-		for (int k = 0; k < l; k++) {
-			int n = frac.num[k];
-			for (int i = k+1; i < l; i++) {
-				n *= frac.den[i];
-			}
-			a += n;
-		}
-	}
-	else if (frac.circle == 0) { // circle on the left
-		a = 1;
-		b = 1;
-		for (int k = 0; k < l; k++) {
-			a *= frac.num[k];
-			b *= frac.den[k];
-		}
-	}
-	else { // circle on the right
-		b = 1;
-		for (int k = 0; k < l; k++) {
-			b *= frac.den[k];
-		}
-
-		a = 0;
-		for (int k = 0; k < l; k++) {
-			int n = frac.num[k];
-			for (int i = 0; i < k; i++) {
-				n *= frac.num[i];
-			}
-
-			for (int i = k+1; i < l; i++) {
-				n *= frac.den[i];
-			}
-			a += n;
-		}
-	}
-
-	result.num = a;
-	result.den = b;
-	return result;
-}
-
-
 int main (void) {
-	setbuf (stdout, NULL);
-
-
+	setbuf (stdout, NULL); // for buggy output
+	
+	
 	char s[100];
 	struct multFRACTION frac;
 
@@ -72,6 +17,7 @@ int main (void) {
 
 	free (frac.num);
 	free (frac.den);
+	fflush (stdout); // for buggy output
 	return 0;
 }
 
