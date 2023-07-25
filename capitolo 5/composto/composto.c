9726a7fc721b09e887b84ece4a0dcb66e01d2941
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-07-25 15:17:02 +0200

    Add files via upload

--- capitolo 5/composto/composto.c
+++ capitolo 5/composto/composto.c
@@ -1,67 +1,56 @@
 #include "../../mylib.h"
 
 
+struct mixedNUMBER division (int n1, int n2) { // divide two numbers and return a mixed number [mix_div in mylib.h]
+	// initialize variables
+	int* denvec = simple_den (factorize (n2)); // factorize the denominator
+	int l = length_vec (denvec);
+
+		printf ("%d = ", n2);
+		for (int k = l-1; k >= 0; k--) {
+			printf ("%d", denvec[k]);
+			printf ( (k != 0) ? "x" : ""); // don't print after the last number
+		}
+		printf ("\n\n");
+
+	struct mixedNUMBER result;
+	result.pfrac.circle = -1;
+	result.pfrac.length = l;
+	result.pfrac.num = calloc (l, sizeof(int));
+	result.pfrac.den = denvec;
+
+	// calculate numerators
+	int m = n1;
+	for (int k = l-1; k >= 0; k--) {
+			printf ("%d", m);
+		result.pfrac.num[k] = m % result.pfrac.den[k];
+		m /= result.pfrac.den[k];
+			printf (" = %d x %d + %d\n", result.pfrac.den[k], m, result.pfrac.num[k]);
+	}
+	result.pint = m;
+
+	return result;
+}
+
 void cast_out_X (int n1, struct mixedNUMBER result) {
 	// initialize variables
 	int l = result.pfrac.length;
-	int* primes = calloc (l, sizeof(int)); // store primes (dynamic programming)
-	int verif, isprime, j = 0, p = 7; // verif = flag controlling the termination of the cycle, isprime = flag checking whether p is prime, j = primes counter (gives position of last encountered prime), p = current prime (start from 7)
-
-	// find a prime number different from the denominators
-	for ( ; 1 ; p++) { // infinite cycle (until broken); at each iteration p gets incremented
-		isprime = 1;
-		if ((p % 2 == 0) || (p % 3 == 0) || (p % 5 == 0)) { // check divisibility by 2,3,5
-			isprime = 0;
-			continue;
-		}
-		for (int k = 0; k < j; k++) { // check divisibility by all other smaller primes
-			if (p % primes[k] == 0) {
-				isprime = 0;
-				break;
-			}
-		}
-
-		if (isprime == 1) {
-			if (j == l) { // checked as many primes as the number of denominators
-				break;
-			}
-
-			// check p is already in the denominators
-			verif = 1;
-			for (int k = 0; k < l; k++) {
-				if (p == result.pfrac.den[k]) {
-					verif = 0;
-					primes[j] = p;
-					j++;
-					break;
-				}
-			}
-
-			if (verif == 1) {
-				break;
-			}
-		}
-	}
+	int p = find_prime (result.pfrac.den, l); // find a prime number different from the denominators
+		printf ("mod %d\n\n", p);
 
 	// calculate remainders
-	int m1 = n1 % p, mr = result.pint % p;
+	int m1 = n1 % p;				printf ("%d %% %d = %d\n", n1, p, m1);
+	int mr = result.pint % p;		printf ("%d -> %d\n", result.pint, mr);
 	for (int k = 0; k < l; k++) {
-		mr *= result.pfrac.den[k];
-		mr += result.pfrac.num[k];
-		mr = mr % p;
+									printf ("%d", mr);
+		mr *= result.pfrac.den[k];	printf (" x %d", result.pfrac.den[k]);
+		mr += result.pfrac.num[k];	printf (" + %d", result.pfrac.num[k]);
+		mr = mr % p;				printf (" -> %d\n", mr);
 	}
 
 	// output
-	printf ("\n\nmod %d: ", p);
-	if (m1 == mr) {
-		printf ("correct");
-	}
-	else {
-		printf ("wrong");
-	}
-
-	// free memory
-	free (primes);
+		printf ("\n");
+		printf ( (m1 == mr) ? "correct" : "wrong");
 }
 
 
@@ -69,19 +58,27 @@ int main (void) {
 	setbuf (stdout, NULL); // for buggy output
 
 
+	// initialize variables
 	int n1, n2;
 
 	scanf ("%d%d", &n1, &n2);
+		printf ("\n----------\n\n");
 
-	struct mixedNUMBER result = mix_div (n1, n2);
-	print_mixedNUMBER (result);
+	// call functions and output
+	struct mixedNUMBER result;
+	result = division (n1, n2);
+		printf ("\n");   print_mixedNUMBER (result);
+		printf ("\n\n----------\n\n");
 
 	cast_out_X (n1, result);
 
 
+	// free memory
 	free (result.pfrac.num);
 	free (result.pfrac.den);
+
 	fflush (stdout); // for buggy output
+
 	return 0;
 }
 
@@ -91,12 +88,16 @@ int main (void) {
 // also check the result by casting out a prime number that is not already present in the factors of the denominator
 
 // INPUT: 2 numbers
-// OUTPUT: 1 mixed number, result of the division
-//         the result of casting out and the modulo chosen
+// OUTPUT: the "rule" of the denominator
+//         the steps of the operation
+//         1 mixed number, result of the operation
+//         the modulo chosen and the remainders
+//         the result of casting out ("correct" or "wrong")
 
 
 
 // examples
+
 
 // 749 / 75 = 2,4,4/3,5,5|9
 //       75 = 3×5×5
@@ -105,4 +106,5 @@ int main (void) {
 //         1760 = 2×8×10×11
 
 // 81540 / 8190 = 3,12/7,13|9
+//             (= 4,2,4,12/7,9,10,13|9)
 //         8190 = 7x9x10x13
