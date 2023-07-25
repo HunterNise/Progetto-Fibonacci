eb2e2f4de7ab9009e7a856b715a8092365bd67e9
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-07-25 15:16:09 +0200

    Add files via upload

--- capitolo 2/moltiplicazione/moltiplicazione.c
+++ capitolo 2/moltiplicazione/moltiplicazione.c
@@ -4,19 +4,18 @@
 struct NUMBER multiplication (struct NUMBER num1, struct NUMBER num2) {
 	// initialize variables
 	struct NUMBER result;
-	int l1 = num1.length, l2 = num2.length;
+	int l1 = num1.length,	    l2 = num2.length;
 	int lmax = l1 + l2; // maximum length of the result
 
-	for (int k = 0; k < lmax-l1; k++) {
-		printf ("0");
-	}
-	print_NUMBER (num1);
-	printf ("\n");
-	for (int k = 0; k < lmax-l2; k++) {
-		printf ("0");
-	}
-	print_NUMBER (num2);
-	printf ("\n\n");
+		for (int k = 0; k < lmax-l1; k++) {
+			printf ("0");
+		}
+		print_NUMBER (num1);	printf ("\n");
+		for (int k = 0; k < lmax-l2; k++) {
+			printf ("0");
+		}
+		print_NUMBER (num2);	printf ("\n");
+		printf ("\n");
 
 	// allocate memory
 	int* d = calloc (lmax, sizeof(int)); // result digits
@@ -31,10 +30,10 @@ struct NUMBER multiplication (struct NUMBER num1, struct NUMBER num2) {
 			c2 = (j < l2) ? num2.digits[j] : 0 ;
 
 			s += c1 * c2;
-			printf ("%dx%d + ", c1, c2, s);
+				printf ("%dx%d + ", c1, c2, s);
 		}
 		s += carry;
-		printf ("%d = %d\n", carry, s);
+			printf ("%d = %d\n", carry, s);
 
 		d[k] = get_last_digit (s);
 		carry = (int) (s / 10);
@@ -52,25 +51,17 @@ struct NUMBER multiplication (struct NUMBER num1, struct NUMBER num2) {
 }
 
 void cast_out_9 (struct NUMBER num1, struct NUMBER num2, struct NUMBER result) { // check the correctness of the operation by casting out nines
-	int m1 = mod9 (num1),
-	    m2 = mod9 (num2),
-		mr = mod9 (result);
+	// calculate remainders
+	int m1 = mod9 (num1),	    m2 = mod9 (num2),	    mr = mod9 (result);
 	int M = (m1 * m2) % 9;
 
-	int n1 = NUMBER_to_int (num1),
-	    n2 = NUMBER_to_int (num2),
-		nr = NUMBER_to_int (result);
-	printf ("%d %% 9 = %d\n", n1, m1);
-	printf ("%d %% 9 = %d\n", n2, m2);
-	printf ("%d %% 9 = %d\n", nr, mr);
+	// output
+		print_NUMBER (num1);	printf (" %% 9 = %d\n", m1);
+		print_NUMBER (num2);	printf (" %% 9 = %d\n", m2);
+		print_NUMBER (result);	printf (" %% 9 = %d\n", mr);
 
-	printf ("\n");
-	if (M == mr) {
-		printf ("correct");
-	}
-	else {
-		printf ("wrong");
-	}
+		printf ("\n");
+		printf ( (M == mr) ? "correct" : "wrong");
 }
 
 
@@ -82,14 +73,13 @@ int main (void) {
 	scanf ("%d%d", &n1, &n2);
 	num1 = init_NUMBER (n1);
 	num2 = init_NUMBER (n2);
-	printf ("\n----------\n\n");
+		printf ("\n----------\n\n");
 
-	// call functions
+	// call functions and output
 	struct NUMBER result;
 	result = multiplication (num1, num2);
-	printf ("\n");
-	print_NUMBER (result);
-	printf ("\n\n----------\n\n");
+		printf ("\n");   print_NUMBER (result);
+		printf ("\n\n----------\n\n");
 
 	cast_out_9 (num1, num2, result);
 
@@ -108,7 +98,7 @@ int main (void) {
 // also check the result by casting out nines
 
 // INPUT: 2 numbers
-// OUTPUT: the steps of the multiplication
+// OUTPUT: the steps of the operation
 //         1 number, result of the operation
 //         the remainders by 9
 //         the result of casting out nines ("correct" or "wrong")
