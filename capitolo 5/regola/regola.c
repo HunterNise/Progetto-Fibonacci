7b13b61ce4c85efc01284492282274c08933c010
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-07-25 15:17:37 +0200

    Add files via upload

--- capitolo 5/regola/regola.c
+++ capitolo 5/regola/regola.c
@@ -1,20 +1,23 @@
 #include "../../mylib.h"
 
 
-void cast_out_9 (int n, int* denvec, int l) {
-	int m = n % 9;
-	int M = 1;
-	for (int k = 0; k < l; k++) {
-		M *= denvec[k];
-		M = M % 9;
+void cast_out_X (int n, int* denvec, int l) {
+	// initialize variables
+	int p = find_prime (denvec, l); // find a prime number different from the denominators
+		printf ("mod %d\n\n", p);
+
+	// calculate remainders
+	int m = n % p;					printf ("%d %% %d = %d\n", n, p, m);
+	int M = denvec[l-1] % p;		printf ("%d -> %d\n", denvec[l-1], M);
+	for (int k = l-2; k >= 0; k--) {
+									printf ("%d x %d -> %d x %d", M, denvec[k], M, denvec[k] % p);
+		M *= denvec[k] % p;			printf (" = %d", M);
+		M = M % p;					printf (" -> %d\n", M);
 	}
 
-	if (M == m) {
-		printf ("\n\ncorrect");
-	}
-	else {
-		printf ("\n\nwrong");
-	}
+	// output
+		printf ("\n");
+		printf ( (M == m) ? "correct" : "wrong");
 }
 
 
@@ -22,40 +25,55 @@ int main (void) {
 	setbuf (stdout, NULL); // for buggy output
 
 
+	// initialize variables
 	int n;
 
 	scanf ("%d", &n);
 
-	int* denvec = simple_den (n); // array of denominators
-	int l = 0;
-	while (denvec[l] != -1) {
-		l++;
+	// call functions and output
+	int* denvec = simple_den (factorize (n)); // factorize the number
+	int l = length_vec (denvec);
+
+	if (l == 1) { // only one factor = prime
+		printf ("%d is prime", n);
 	}
-	
-	printf ("%d", n);
-	printf (" = ");
-	print_vec_rev (denvec, l);
+	else {
+		printf ("%d = ", n);
+		for (int k = l-1; k >= 0; k--) {
+			printf ("%d", denvec[k]);
+			printf ( (k != 0) ? "x" : ""); // don't print after the last number
+		}
+		printf ("\n\n----------\n\n");
 
-	cast_out_9 (n, denvec, l);
+		cast_out_X (n, denvec, l);
+	}
 
 
+	// free memory
 	free (denvec);
+
+	fflush (stdout); // for buggy output
+
 	return 0;
 }
 
 
 
 // find the "rule" of a number
-// this is equivalent to finding his prime factors (and beautify the factors)
-// also check the result by casting out nines
+//   this is equivalent to finding his prime factors (and beautify the factors)
+// also check the result by casting out a prime number that is not already present in the factors
 
 // INPUT: 1 number
-// OUTPUT: the sequence of factors
-//         "correct" if the cast out 9 test is passed, "wrong" otherwise
+// OUTPUT*: the sequence of factors
+//          the modulo chosen and the remainders
+//          the result of casting out ("correct" or "wrong")
+
+// * if the number is prime the only ouptut is a message saying that
 
 
 
 // examples
+
 
 // 805 = 5×7×23
 
