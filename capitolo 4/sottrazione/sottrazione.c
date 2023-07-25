d10fdc79467a2ca187fc7ca2571c6404970487cc
HunterNise <55357132+HunterNise@users.noreply.github.com>
2023-07-25 15:16:46 +0200

    Add files via upload

--- capitolo 4/sottrazione/sottrazione.c
+++ capitolo 4/sottrazione/sottrazione.c
@@ -4,32 +4,31 @@
 struct NUMBER subtraction (struct NUMBER num1, struct NUMBER num2) {
 	// initialize variables
 	struct NUMBER result;
-	int l1 = num1.length, l2 = num2.length;
+	int l1 = num1.length,	    l2 = num2.length;
 	int lmax = l1; // maximum length of the result
 
-	for (int k = 0; k < lmax-l1; k++) {
-		printf ("0");
-	}
-	print_NUMBER (num1);
-	printf ("\n");
-	for (int k = 0; k < lmax-l2; k++) {
-		printf ("0");
-	}
-	print_NUMBER (num2);
-	printf ("\n\n");
+		for (int k = 0; k < lmax-l1; k++) {
+			printf ("0");
+		}
+		print_NUMBER (num1);	printf ("\n");
+		for (int k = 0; k < lmax-l2; k++) {
+			printf ("0");
+		}
+		print_NUMBER (num2);	printf ("\n");
+		printf ("\n");
 
 	// allocate memory
 	int* d = calloc (lmax, sizeof(int)); // result digits
 
 	// calculate result digits
-	int borrow = 0, carry = 0, c1 = 0, c2 = 0, s = 0;
+	int borrow = 0, carry = 0, c1 = 0, c2 = 0, s = 0; // c1 = current digit of the first number (idem c2 for the second one), s = partial difference of current digits (included the carry)
 	for (int k = 0; k < lmax; k++) {
 		c1 = (k < l1) ? num1.digits[k] : 0 ; // check if it's within the boundaries of the array
 		c2 = (k < l2) ? num2.digits[k] : 0 ;
 
 		borrow = (c1 < c2) ? 1 : 0; // check if borrow is needed
 		s = (c1 + borrow*10) - (c2 + carry);
-		printf ("%d - %d = %d\n", c1 + borrow*10, c2 + carry, s);
+			printf ("%d - %d = %d\n", c1 + borrow*10, c2 + carry, s);
 
 		d[k] = get_last_digit (s);
 		carry = borrow;
@@ -47,9 +46,8 @@ struct NUMBER subtraction (struct NUMBER num1, struct NUMBER num2) {
 }
 
 void cast_out_9 (struct NUMBER num1, struct NUMBER num2, struct NUMBER result) { // check the correctness of the operation by casting out nines
-	int m1 = mod9 (num1),
-	    m2 = mod9 (num2),
-		mr = mod9 (result);
+	// calculate remainders
+	int m1 = mod9 (num1),	    m2 = mod9 (num2),	    mr = mod9 (result);
 
 	int M;
 	if (m1 >= m2) {
@@ -59,20 +57,13 @@ void cast_out_9 (struct NUMBER num1, struct NUMBER num2, struct NUMBER result) {
 		M = ((m1 + 9) - m2) % 9;
 	}
 
-	int n1 = NUMBER_to_int (num1),
-	    n2 = NUMBER_to_int (num2),
-		nr = NUMBER_to_int (result);
-	printf ("%d %% 9 = %d\n", n1, m1);
-	printf ("%d %% 9 = %d\n", n2, m2);
-	printf ("%d %% 9 = %d\n", nr, mr);
+	// output
+		print_NUMBER (num1);	printf (" %% 9 = %d\n", m1);
+		print_NUMBER (num2);	printf (" %% 9 = %d\n", m2);
+		print_NUMBER (result);	printf (" %% 9 = %d\n", mr);
 
-	printf ("\n");
-	if (M == mr) {
-		printf ("correct");
-	}
-	else {
-		printf ("wrong");
-	}
+		printf ("\n");
+		printf ( (M == mr) ? "correct" : "wrong");
 }
 
 
@@ -84,21 +75,20 @@ int main (void) {
 	scanf ("%d%d", &n1, &n2);
 	num1 = init_NUMBER (n1);
 	num2 = init_NUMBER (n2);
-	printf ("\n----------\n\n");
+		printf ("\n----------\n\n");
 
 	// check the input
 	int verif = geq (num1, num2);
-	if (!verif) {
-		printf ("Impossibile eseguire la sottrazione."); // negative numbers are not contemplated
+	if (verif != 1) {
+		printf ("Cannot subtract."); // negative numbers are not contemplated
 		return 0;
 	}
 
-	// call functions
+	// call functions and output
 	struct NUMBER result;
 	result = subtraction (num1, num2);
-	printf ("\n");
-	print_NUMBER (result);
-	printf ("\n\n----------\n\n");
+		printf ("\n");   print_NUMBER (result);
+		printf ("\n\n----------\n\n");
 
 	cast_out_9 (num1, num2, result);
 
@@ -118,7 +108,7 @@ int main (void) {
 // also check the result by casting out nines
 
 // INPUT: 2 numbers
-// OUTPUT: the steps of the subtraction
+// OUTPUT: the steps of the operation
 //         1 number, result of the operation
 //         the remainders by 9
 //         the result of casting out nines ("correct" or "wrong")
